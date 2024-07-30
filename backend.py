c706139dd5d0f4bbc3959c1406725eac3dcd5371
Renato <renato92.hora@gmail.com>
2024-07-30 11:45:39 -0300

    Finished setting endpoint from api weather

---
+++ backend.py
@@ -0,0 +1,13 @@
+import requests
+
+API_KEY = "141710af2113bab9f55ef73e1bcd33d5"
+
+def get_data(place, forecast_days=None, kind=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    return data
+
+if __name__ == "__main__":
+    print(get_data(place="Tokyo"))
+
--- exercises/exercise1/get_data.py
+++ exercises/exercise1/get_data.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 
 def get_data(colum_name):
-    df = pd.read_csv("exercise/happy.csv")
+    df = pd.read_csv("exercises/exercise1/happy.csv")
     colum_name = colum_name.lower()
     return df[colum_name]
